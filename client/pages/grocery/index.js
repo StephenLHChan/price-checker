c8368710306aaa0214a32a6901930763af756ecb
StephenLHChan <stephenc.1018@gmail.com>
2022-06-02 22:01:19 -0400

    Amend page: grocery

--- client/pages/grocery/index.js
+++ client/pages/grocery/index.js
@@ -2,6 +2,7 @@ import { useState, useCallback } from 'react';
 import { getSession } from 'next-auth/react';
 import axios from 'axios';
 import { Box, Container, Typography } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 import Input from '../../components/input';
 import ResultTable from '../../components/result-table';
@@ -34,31 +35,44 @@ const Grocery = () => {
   return (
     <Container>
       <Box>
-        <Typography p={4}>Search for Grocery</Typography>
-        <Input searchItem={searchItem} p={4} />
-        {displayResult && <ResultTable data={result} p={4}/>}
+        <Box display={'inline-flex'}>
+          <SearchIcon sx={{ fontSize: 80 }} />
+          <Typography variant="h3" p={2}>
+            Search for price...
+          </Typography>
+        </Box>
+
+        <Box p={2}>
+          <Input searchItem={searchItem} />
+        </Box>
+        {displayResult && (
+          <Box p={2}>
+            <ResultTable data={result} />
+          </Box>
+        )}
       </Box>
     </Container>
   );
 };
 
 export async function getServerSideProps(context) {
-  const session = await getSession(context)
+  const session = await getSession(context);
 
-  if (!session){
+  if (!session) {
     return {
       redirect: {
-        destination: '/api/auth/signin?callbackUrl=http://localhost:3000/grocery',
+        destination:
+          '/api/auth/signin?callbackUrl=http://localhost:3000/grocery',
         permanent: false
       }
-    }
+    };
   }
 
   return {
     props: {
-      session,
+      session
     }
-  }
+  };
 }
 
 export default Grocery;
